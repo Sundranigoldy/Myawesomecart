6ac9883e6fb8dea44f2dabb01be752df0b4e4984
sundranigoldy <sundranigoldy@gmail.com>
2022-12-14 21:37:45 +0530

    images and 1st time

--- shop/views.py
+++ shop/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from math import ceil
+from .models import Product #to import all things from database
 
 # Create your views here.
 def index(request):
-    return render(request, 'shop/index.html')
+    products= Product.objects.all() # fetching data from database..
+    allProds=[]
+    catprods= Product.objects.values('category', 'id') # to diffn from category wise and id wise
+    cats= {item["category"] for item in catprods} #catgeory in set compreehensation
+    for cat in cats:
+        prod=Product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4)) #to calculate no of slide and ceil funct comes from math
+        allProds.append([prod, range(1, nSlides), nSlides]) #taking an range func so that i can use value in template func as it return generator
+
+    params={'allProds':allProds } #passing an template with name product
+    return render(request,"shop/index.html", params)
 
 def about(request):
-    return HttpResponse('We are at about')
+    return render(request, 'shop/about.html')
 
 def contact(request):
     return HttpResponse('We are at contact')         
